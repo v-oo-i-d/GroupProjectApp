0841864650df470b8b74cc9e676e4959c9212897
Kobe Spring <knorrisspring@gmail.com>
2025-05-15 12:49:05 +1200

    To be honest I'm not sure any extensive cleaning is required. Aside from what has been performed already.

--- data/cleaning_data.py
+++ data/cleaning_data.py
@@ -2,8 +2,31 @@ import pandas as pd
 
 reg_season_df = pd.read_csv("data/NBA_Regular_Season_Games_2014_2025.csv")
 play_off_df = pd.read_csv("data/NBA_Playoff_Games_2014_2025.csv")
+player_info = pd.read_csv("data/NBA_Player_Info_And_Stats_2014_2025.csv")
 
 
-print(reg_season_df.dtypes)
-print(play_off_df.dtypes)
-#can confirm that the data type of each column is appropriate for the data
+reg_season_df['GAME_DATE'] = pd.to_datetime(reg_season_df['GAME_DATE'], errors='coerce')
+
+print(reg_season_df['GAME_DATE'].dtypes)
+print(reg_season_df['GAME_DATE'].head())
+
+play_off_df['GAME_DATE'] = pd.to_datetime(play_off_df['GAME_DATE'], errors='coerce')
+
+print(play_off_df['GAME_DATE'].dtypes)
+print(play_off_df['GAME_DATE'].head())
+#can confirm that the data type of each column is appropriate for the data (now)
+
+print(reg_season_df.isna().sum())
+print(player_info.isna().sum())
+print(play_off_df.isna().sum())
+#identified some emtpy rows <10 so just going to remove them :)
+
+reg_season_df_clean = reg_season_df.dropna()
+play_off_df_clean = play_off_df.dropna()
+player_info = player_info.dropna()
+
+print(reg_season_df.duplicated().sum())
+print(play_off_df.duplicated().sum())
+print(player_info.duplicated().sum())
+#no duplicated rows.
+
